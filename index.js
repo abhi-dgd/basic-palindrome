f1bf413b002a61c950781633ebf758f737ab4a9c
Abhishek Degadwala <abhidegadwala@gmail.com>
2024-07-06 21:09:45 -0400

    feat(isPlaindrome):add check functionality
    rename old display function to isPalindrome and add checks
    add reverseInput function to reverse the given string
    update html to use update isPalindrome func

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 let input = document.getElementById("check")
 
-const displayResult = () => {
+const isPalindrome = () => {
     let entered = input.value;
-    alert(entered);
+    reversed = reverseInput(entered)
+    if (entered != reversed){
+        alert("Is not palindrome")
+    } else {
+        alert("Is a palindrome!")
+    }
+};
+
+const reverseInput = (word) => {
+    // Split word into array elements
+    // reverse the order of elements
+    // concat back into a string
+    return word.split("").reverse().join("")
 };
